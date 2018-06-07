a4100074340556b40c5741b6f7b8b11b75eaa09c
Furkan Enes POLATOĞLU <furkanenes1160@gmail.com>
2018-06-07 15:11:23 +0300

    Add files via upload

---
+++ Odev_24-02/src/Odev_24_02.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Odev_24_02 {
+public static void main(String[] args){
+
+Scanner scan = new Scanner(System.in);
+int islem;
+System.out.println("Yapacaðýnýz Ýþlemi Seçiniz : ");
+System.out.println("Toplama:1 Çýkarma:2 Çarpma:3 Bölme:4");
+islem = scan.nextInt();
+
+
+int a;
+System.out.print("1.Sayýyý giriniz: ");
+a = scan.nextInt();
+int b;
+System.out.print("2.Sayýyý giriniz: ");
+b = scan.nextInt();
+
+if (islem == 1)
+{
+	System.out.print("Ýþlemin sonucu: " + (a + b));
+}
+
+else if (islem == 2)
+{
+	System.out.print("Ýþlemin Sonucu:" + (a - b));
+}
+
+else if (islem == 3)
+{
+	System.out.print("Ýþlemin Sonucu: " + (a * b));
+}
+
+else if (islem == 4)
+{
+	System.out.print("Ýþlemin Sonucu: " + (a / b));
+}
+else
+{
+	System.out.println ("Bilinmeyen Ýþlem");
+}
+}
+}
